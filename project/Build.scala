1ca1dca8d27cd746b2b9019b0303a44202f87d8f
Mike Bryant <mikesname@gmail.com>
2012-06-10 16:11:33 +0100

    Added dependencies for Fakod's neo4j-scala lib (which needs to be built and installed to the local Maven repository.)

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,18 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.github.seratch" %% "scalikesolr" % "(3.6,)"
+      "org.neo4j" % "neo4j-kernel" % "1.7.M01" % "test" classifier "tests",
+      "org.neo4j" % "neo4j" % "1.7.M01",
+      "org.neo4j" % "neo4j-rest-graphdb" % "1.8-SNAPSHOT",
+      "org.neo4j" % "neo4j-shell" % "1.7.M01",
+      "com.github.seratch" %% "scalikesolr" % "(3.6,)",
+      "org.neo4j" % "neo4j-scala" % "0.2.0-M1"
     )
 
-
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      resolvers += (
+          "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
+      )
     )
 
 }
