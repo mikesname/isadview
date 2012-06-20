e56c1dbd0c14e38a79702a6f2e324757cda9a6cb
Mike Bryant <mikesname@gmail.com>
2012-06-20 15:39:20 +0100

    Add some config to test SQL DB connection.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,4 +7,15 @@ object Application extends Controller {
   def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def dbtest = Action { implicit request =>
+    import play.api.db.DB
+    import anorm.SQL
+    import play.api.Play.current
+    val result = DB.withConnection { implicit c =>
+      var query = SQL("SELECT DISTINCT email FROM auth_user WHERE username = {user}").on("user" -> "mike")
+      query.apply().head[String]("auth_user.email")
+    }
+    Ok("Result: %s".format(result))
+  }
 }
