535e0a51838e088f4c82f147ddf84320112415fc
Mike Bryant <mikesname@gmail.com>
2012-07-03 17:17:26 +0100

    Add generalised query method

--- groovy/gremlin.groovy
+++ groovy/gremlin.groovy
@@ -109,6 +109,83 @@ def delete_vertex_with_related(_id, outRels, inRels) {
   }
 }
 
+
+// construct a query - several of these paths can probably
+// be optimised, and I'm not sure if the index is being
+// used as much as it could be.
+def query(index_name, inrels, outrels, filters, high, low, order_by, docount, dodelete) {
+  try {
+    def opblocks = [
+      "exact":   {it, a, v -> it."$a" == v},
+      "iexact":  {it, a, v -> it."$a".toLowerCase() == v.toLowerCase()},
+      "contains":{it, a, v -> it."$a".matches(".+$v.+")},
+      "startswith":{it, a, v -> it."$a".matches("^$v.+")},
+      "endswith":{it, a, v -> it."$a".matches(".+$v\$")},
+      "isnull":  {it, a, v -> it."$a" == null},
+      "gt":      {it, a, v -> it."$a" >  v},
+      "lt":      {it, a, v -> it."$a" <  v},
+      "gte":     {it, a, v -> it."$a" >= v},
+      "lte":     {it, a, v -> it."$a" <= v},
+    ]
+
+    def pipe = g.V._()
+
+    for (relation in inrels) {
+      def (relname, outV) = relation
+      pipe = pipe.retain(g.v(outV).inE(relname).outV.toList())
+    }
+
+    for (relation in outrels) {
+      def (relname, outV) = relation
+      pipe = pipe.retain(g.v(outV).outE(relname).inV.toList())
+    }
+
+    if (index_name != null)
+      pipe = pipe.filter{it.element_type==index_name}
+
+    for (filter in filters) {
+      def (attr, op, value) = filter
+
+      def opblock = opblocks.get(op)
+      if (opblock == null)
+        throw new Exception("Unsupported filter operator: '$op'")
+      pipe = pipe.filter{opblock(it, attr, value)}
+    }
+    
+    if (!docount && low != null && high != null) {
+      if (low != 0 && high == null)
+        pipe = pipe.range(low, -1)
+      else if (low == 0 && high != null)
+        pipe = pipe.range(0, high)
+      else if (low > 0 && high != null)
+        pipe = pipe.range(low, high)
+    }
+  
+    if (order_by != null && order_by) {
+      for (order in order_by) {
+        def (attr, desc) = order
+        pipe = pipe.sort{it."$attr"}
+        if (desc)
+          pipe = pipe.reverse()
+      }
+    }
+
+    if (docount)
+      return pipe.count()
+
+    if (dodelete) {
+      pipe.collect{g.removeVertex(it)}
+      return true;
+    } 
+    return pipe
+  } catch (e) {
+    return e
+  }
+}
+
+
+
+
 /* Update a vertex and all it's subordinate relations.
  * These are vertices with the given outgoing relationships
  * pointing to the parent node:
