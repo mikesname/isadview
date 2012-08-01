6dc20768c67289c20cc5200a190f0774358af3e8
Mike Bryant <mikesname@gmail.com>
2012-08-01 10:06:14 +0100

    Add a view function for rendering pretty dates.

--- app/views/Helpers.scala
+++ app/views/Helpers.scala
@@ -1,4 +1,20 @@
+package views
+
 
 import views.html.helper.FieldConstructor
 
+import org.ocpsoft.pretty.time.PrettyTime
 
+object Helpers {
+  def prettyDate(d: org.joda.time.DateTime): String = {
+    val p = new PrettyTime() // TODO: Locale awareness...
+    p.format(d.toDate)
+  }
+
+  def prettyDate(d: Option[org.joda.time.DateTime]): String = {
+    d match {
+      case Some(dt) => prettyDate(dt)
+      case None => ""
+    }
+  }
+}
