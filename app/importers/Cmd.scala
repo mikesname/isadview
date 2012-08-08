057f26f3ef1e7ab27acb92fd31328ca5a69e0f94
Mike Bryant <mikesname@gmail.com>
2012-08-08 15:05:12 +0100

    Work towards command-line xmlToGeoff

--- app/importers/Cmd.scala
+++ app/importers/Cmd.scala
@@ -1,21 +1,21 @@
 package importers
 
+import scalax.io.{Resource,Codec => IOCodec}
 import play.api.libs.concurrent.execution.defaultContext
 
-object Cmd {
+object XmlToGeoff {
+  def repositoryId(repo: models.Repository) = "repo%d".format(repo.id)
 
-  def doImport(importer: Importer[xml.NodeSeq], format: String, file: String, repoId: String) = {
-    XMLPullParser.processSource(format, io.Source.fromFile(file)) { doc =>
-      importer.docToGeoff(repoId, doc).map { line =>
-        println(line)
-      }
+  def getGeoff(importer: Importer[xml.NodeSeq], format: String, infile: String, outfile: String, repoId: String) = {
+    val output = Resource.fromFile(outfile)
+    output.truncate(0)
+    XMLPullParser.processSource(format, io.Source.fromFile(infile)) { doc =>
+      output.writeStrings(importer.docToGeoff(repoId, doc), separator="\n")(IOCodec.UTF8)
     }
   }
 
-  def repositoryId(repo: models.Repository) = "repo%d".format(repo.id)
-
   def main(args: Array[String]): Int = args match {
-    case Array(repo, format, file) => {
+    case Array(repo, format, infile, outfile) => {
       models.Repository.fetchByFieldOption("slug", repo).map { repooption =>
         repooption match {
           case Some(repository) => {
@@ -24,15 +24,15 @@ object Cmd {
               case "doc" => USHMM
               case x => sys.error("Unknown importer format: %s".format(x))
             }
-            doImport(importer, format, file, repositoryId(repository))
+            getGeoff(importer, format, infile, outfile, repositoryId(repository))
           }
           case None => sys.error("Unable to find repository with slug: %s".format(repo))
         }
       }
       0
     }
-    case _ => {
-      println("Usage: <repository-slug> <format> <file>")
+    case _ => {  
+      println("Usage: <repository-slug> <format> <infile> <outfile>")
       1
     }
   }
