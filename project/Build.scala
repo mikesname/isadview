3779cebb752077968ac72fb61d3848a3433addb7
Mike Bryant <mikesname@gmail.com>
2012-08-10 15:02:14 +0100

    Tweak config to handle Scala 2.9.2 (latest play.2.0 is on that now.)

--- project/Build.scala
+++ project/Build.scala
@@ -9,12 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "com.github.seratch" %% "scalikesolr" % "3.6.0",
+      "net.databinder" %% "dispatch-http" % "0.8.8" withSources,
       "org.scala-tools.time" % "time_2.9.1" % "0.5",
-      "net.liftweb" %% "lift-json-ext" % "2.4-M5",
+      "net.liftweb" % "lift-json-ext_2.9.1" % "2.4-M5",
       "org.codehaus.groovy" % "groovy-all" % "1.8.6",
       "postgresql" % "postgresql" % "9.1-901.jdbc4",
-      "jp.t2v" %% "play20.auth" % "0.3-SNAPSHOT",
-      "org.ocpsoft.prettytime" % "prettytime" % "1.0.8.Final"
+      "org.ocpsoft.prettytime" % "prettytime" % "1.0.8.Final",
+      "jp.t2v" % "play20.auth_2.9.1" % "0.3-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
